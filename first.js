96ac34e8570b314954157a752e408c2c629eed01
Amish-mach14 <Amishmishra77@gmail.com>
2024-06-11 23:53:26 +0530

    Did some changes in js

--- first.js
+++ first.js
@@ -1,11 +1,38 @@
-let btn = document.querySelector("#btn");
-let div = document.querySelector("body");
-let currentmode = "light";
-btn.addEventListener("click", () => {
-    if(currentmode === "light") {
-        currentmode = "dark";
-        div.style.backgroundColor = "black";
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll(".choice");
+
+const genComputerChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randix = Math.floor(Math.random() * 3);
+    return options[randix];
+}
+
+const drawGame = () => {
+    console.log("game was a draw");
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+    const compChoice = genComputerChoice();
+    console.log("comp choice = ", compChoice);
+    let userWin=true;
+
+    if(userChoice === compChoice) {
+        drawGame();
+    } else if (userChoice === "paper") {
+        userWin = (compChoice == "scissors") ? false:true;
+    } else if (userChoice === "rock") {
+        userWin = (compChoice == "paper") ? false:true;
     } else {
-        currentmode = "light";
-        div.style.backgroundColor = "white";
-    }})
+        userWin = (compChoice == "rock") ? false:true;
+    }
+}; 
+
+choices.forEach((choice) => {
+    console.log(choice);
+    choice.addEventListener("click", () => {
+        const userchoice = choice.getAttribute("id");
+        console.log("choice was clicked", userchoice);
+    });
+});
